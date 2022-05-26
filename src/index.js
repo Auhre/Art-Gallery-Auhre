984317534f644a3641fc1e6ed6d506dc7b72cd47
Don Vito L. Alzaga <donvitoalzaga.se@gmail.com>
2022-05-26 11:22:26 +0800

    my commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Page from './page';
+import Page from './Page';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
